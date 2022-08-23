3f6c19fe4bb919f3177b604ec8688261aa771ddd
clerison <cosclerison@gmail.com>
2022-08-23 15:20:30 -0300

    Edit header

--- src/App.js
+++ src/App.js
@@ -6,9 +6,25 @@ import { BsTrash, BsBookmarkCheck, BsBookmarkCheckfill } from "react-icons/bs";
 const API = "http://localhost:5000";
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [time, setTime] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(false);
+
   return (
     <div className="App">
-      <h1>List ToDo</h1>
+      <div className="todo-header">
+        <h1>React ToDo</h1>
+      </div>
+      <div className='form-todo'>
+        <p>Formulario</p>
+      </div>
+      <div className='list-todo'>
+        <p>Lista</p>
+      </div>
+
+      
+
     </div>
   );
 }
