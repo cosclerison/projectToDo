97145094794ebaca33c08a5dd3e46c43ba7c2bb2
clerison <cosclerison@gmail.com>
2022-08-23 14:01:40 -0300

    configurando dependencias e API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import './App.css';
 
+import { useState, useEffect } from "react";
+import { BsTrash, BsBookmarkCheck, BsBookmarkCheckfill } from "react-icons/bs";
+
+const API = "http://localhost:5000";
+
 function App() {
   return (
     <div className="App">
